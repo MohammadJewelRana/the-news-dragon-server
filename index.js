6a469e6f63e6189e13cc7ea6bca604399d9118b6
MohammadJewelRana <js.rana0326@gmail.com>
2023-08-08 22:34:00 +0600

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors=require('cors')
 app.use(cors())
 
 const categories=require('./data/categories.json')
-
+const news=require('./data/news.json')
 
 app.get('/',(req,res)=>{
     res.send('Dragon is running')
@@ -16,6 +16,37 @@ app.get('/',(req,res)=>{
 app.get('/categories',(req,res)=>{
     res.send(categories)
 })
+
+//direct news
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+//dynamic 
+//based on id
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+     const selectedNews=news.find(n=>n._id===id);
+     res.send(selectedNews);
+})
+
+//based on category
+app.get('/categories/:id',(req,res)=>{
+    const id= parseInt(req.params.id);
+    if(id===0){
+        res.send(news);
+    }
+    else{
+        const categoryNews=news.filter(n=> parseInt(n.category_id)===id);
+        res.send(categoryNews);
+   }
+
+})
+ 
+
+
+
+
 
 
 app.listen(port,()=>{
